bc085b2b46718cdafe268aafc0ff6d5a3280a842
Hrach <steepgaskin@gmail.com>
2022-06-24 21:15:59 +0400

    fwrd-v.0.11

--- src/components/FAQ/dataFAQ.js
+++ src/components/FAQ/dataFAQ.js
@@ -1,57 +1,66 @@
 import React from 'react'
 
 const data = [
-	{
-		question: 'WHAT ARE ELEMENTALS?',
-		answer: (
-			<>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vel
-					augue eu lorem euismod cursus sit amet at massa.
-				</p>
-			</>
-		),
-	},
-	{
-		question: 'WHAT IS THE SOCIAL SPA?',
-		answer: (
-			<>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vel
-					augue eu lorem euismod cursus sit amet at massa.
-				</p>
-			</>
-		),
-	},
-	{
-		question: 'WHY NFTS?',
-		answer: (
-			<>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vel
-					augue eu lorem euismod cursus sit amet at massa.
-				</p>
-			</>
-		),
-	},
-	{
-		question: 'WHAT’S THE VIBE OF ELEMENTALS?',
-		answer: (
-			<>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vel
-					augue eu lorem euismod cursus sit amet at massa.
-				</p>
-			</>
-		),
-	},
+  {
+    question: 'WHAT ARE ELEMENTALS?',
+    answer: (
+      <>
+        <p>
+          Elementals is a collection of 10,000 NFTs on the Ethereum blockchain
+          that gives access to its hodlers to the web3 social club and retreat -
+          The Social Spa
+        </p>
+      </>
+    ),
+  },
+  {
+    question: 'WHAT IS THE SOCIAL SPA?',
+    answer: (
+      <>
+        <p>
+          The Social Spa is a web3 project that creates meaning for the new
+          world we’ve entered. We provide tools, products and education to
+          inspire creatives, to heal old wounds and to bring together the next
+          community of leaders and world changers.
+        </p>
+      </>
+    ),
+  },
+  {
+    question: 'WHY NFTS?',
+    answer: (
+      <>
+        <p>
+          NFTs are a fun and dependable way to give our hodlers access to all
+          the features our community has to offer. Elementals live forever on
+          the blockchain, ownership can always be verified with full
+          transparency.
+        </p>
+      </>
+    ),
+  },
+  {
+    question: 'WHAT’S THE VIBE OF ELEMENTALS?',
+    answer: (
+      <>
+        <p>
+          We want everyone to have fun and relax. Our vibe is compassionate,
+          optimistic and a bit different. We don’t fit it and we like it that
+          way. And everyone’s always welcome.
+        </p>
+      </>
+    ),
+  },
   {
     question: 'WHEN MINT?',
     answer: (
       <>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vel
-          augue eu lorem euismod cursus sit amet at massa.
+          Elementals arrive precisely when they’re meant to. The work we do for
+          the space and the community is what matters most, therefore we put
+          inspiration, transformation and connection first. Everything we create
+          as a community will be shared with the world, so that we can win as a
+          collective. Because that’s the most fun way.
         </p>
       </>
     ),
@@ -61,8 +70,12 @@ const data = [
     answer: (
       <>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vel
-          augue eu lorem euismod cursus sit amet at massa.
+          The primary purpose of Elementals is to benefit the human collective
+          by delivering a positive impact at an individual level. We host guided
+          activations to inspire the collective. We utilize decentralized
+          governance protocols to keep the soul aligned. We deliver nourishment
+          for the body that manifests sustainable growth. We’ll grow together.
+          Always listening to you.
         </p>
       </>
     ),
